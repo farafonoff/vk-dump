9b751b5de322f848abdc429663d4ef9d3c0916fd
Paval Lubiecki <paval.lubiecki@yandex.by>
2018-05-21 12:43:39 +0300

    какая-никакая рудиментарная обработка вложений для постов на стене

--- methods.rb
+++ methods.rb
@@ -171,11 +171,58 @@ def make_footer(post)
   "-- Likes: #{likes_count}, Reposts: #{reposts_count} --"
 end
 
+def get_photo_url(attachment)
+  resolution_strings = attachment['photo'].keys.find_all {|str| str.include? 'photo_'}
+  max_res = resolution_strings.map {|str| str.scan(/photo_([0-9]+)/).first.first.to_i }.max
+  photo_url = attachment['photo']["photo_#{max_res}"]
+
+  "#{photo_url}"
+end
+
+def process_attachment(attachment)
+  case attachment['type']
+  when 'photo'
+    url = get_photo_url(attachment)
+    
+    return "img: #{url}"
+  when 'link'
+    url = attachment['link']['url']
+    title = attachment['link']['title']
+    
+    return "link: #{url} (#{title})"
+  when 'audio'
+    artist = attachment['audio']['artist']
+    title = attachment['audio']['title']
+      
+    return "audio: #{artist} - #{title}"
+  when 'video'
+    title = attachment['video']['title']
+
+    return "video: #{title}"
+  when 'doc'
+    url = attachment['doc']['url'] 
+    title = attachment['doc']['title']
+
+    "doc: #{url} (#{title})"
+  else
+    return 'unknown type'
+  end
+end
+
+def process_attachments(post)
+  attachments_count = post['attachments'].count
+  
+  attachments_txt = post['attachments'].map do |curr|
+    process_attachment(curr)
+  end.join("\n")
+
+  "-- Attachments: #{attachments_count} --" + "\n" + attachments_txt
+end
+
 def make_post(post)
   if post['attachments']
-    attachments_count = post['attachments'].count
-    attachments = "-- Attachments: #{attachments_count} --"
-
+    attachments = process_attachments(post)
+    
     return [ make_header(post), post['text'], attachments, make_footer(post) ].join("\n")
   end
 
