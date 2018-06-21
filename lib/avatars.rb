6239d1c84f40538e1480b4e603db9c5e040edfe9
Paval Lubiecki <paval.lubiecki@yandex.by>
2018-06-21 13:53:45 +0300

    вывод идентификаторов пользователей, которые лайкнули аватарки

--- lib/avatars.rb
+++ lib/avatars.rb
@@ -5,12 +5,22 @@ def get_avatar_txt(avatar)
   comments_count = avatar['comments']['count']
   text = avatar['text'].to_s
   date = get_time_txt(avatar['date'])
+  id = avatar['id']
 
   out = ''
   out += "url: #{url}\n"
   out += "date: #{date}\n"
   out += "text: #{text}\n" unless text.empty?
   out += "likes: #{likes_count}\n"
+
+  if (likes_count > 0)
+    likes_user_ids = @vk.likes.getList(type: 'photo', item_id: id)['items']
+
+    out += "like user ids: #{likes_user_ids.join(", ")}\n"
+
+    sleep(@config['sleep_time'])
+  end
+
   out += "reposts: #{reposts_count}\n"
   out += "comments: #{comments_count}\n"
 
